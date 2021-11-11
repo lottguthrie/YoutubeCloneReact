71fd40a2e2f3e2050af056a057ec7062d80bad83
LatoyaNixon <nixonlatoya.ln@gmail.com>
2021-11-11 11:48:08 -0500

    Created Youtube home page and search bar.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useEffect } from 'react';
+import { Search } from './component/search';
 
-function App() {
+
+export function App() {
+  const [currentChannelId, setCurrentChannelId] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>YouTube Channel</h1>
+      <Search setCurrentChannelId={id => setCurrentChannelId(id)}/>
     </div>
   );
 }
 
-export default App;
+ 
+
+
+
+
+
+
+export default App
